748aa45833fe9aff9445780407c9e921807ebd24
RoseUwU12 <122943402+RoseUwU12@users.noreply.github.com>
2023-03-20 13:40:35 -0400

    Create Print.java

---
+++ Print.java
@@ -0,0 +1,15 @@
+public class Print {
+  private int PrintUntil; 
+  private int PrintINT;
+
+  public Print(){
+    PrintINT = 0;
+    PrintUntil = 20;
+  }
+  public int GetPrintInt(){
+    return PrintINT;
+  }
+  public int GetPrintUntil(){
+    return PrintUntil;
+  }
+}
